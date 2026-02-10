fde815fd285e0376a6d870b79810fbf1bcce03a5
Erik Forsgren <crapmannen@hotmail.com>
2026-02-10 19:23:17 +0100

    Add MoveCard use case

--- src/Kanban.Application/Boards/UseCases/MoveCard.cs
+++ src/Kanban.Application/Boards/UseCases/MoveCard.cs
@@ -1,7 +1,28 @@
-﻿namespace Kanban.Application.Boards.UseCases
-{
-    internal class MoveCard
-    {
+﻿using Kanban.Application.Boards.Contracts;
+using Kanban.Application.Boards.Ports;
 
+namespace Kanban.Application.Boards.UseCases
+{
+    public sealed class MoveCard
+    {
+        private readonly IBoardRepository _boardRepository;
+        
+        public MoveCard(IBoardRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+        public async Task<BoardDto?> Handle(
+            Guid boardId, 
+            Guid cardId, 
+            Guid targetColumnId, 
+            CancellationToken ct = default)
+        {
+            var board = await _boardRepository.GetById(boardId, ct);
+            if (board is null) return null;
+
+            board.MoveCard(cardId, targetColumnId);
+            await _boardRepository.Save(board, ct);
+            return BoardDto.From(board);
+        }
     }
 }
